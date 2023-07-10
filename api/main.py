d44c209b7e8097013fa0ce365014b2bec089309d
paavansundar <paavan.duvvuri@gmail.com>
2023-07-10 14:50:30 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,21 +1,12 @@
-import inspect
 from fastapi import FastAPI
 import uvicorn
-from bikeshare_model.predict import make_prediction
-from bikeshare_model import __version__ as model_version
+
 
 app = FastAPI()
 
 @app.get("/predict/")
 def predict():
-    frame = inspect.currentframe()
-    try:
-        data = {'dteday': '2012-11-6', 'season': 'winter', 'hr': '6pm', 'holiday': 'No', 'weekday': 'Tue',
-               'workingday': 'Yes', 'weathersit': 'Clear', 'temp': 16.0, 'atemp': 17.5, 'hum': 30, 'windspeed': 10.0, 'yr': 2012, 'mnth': 'january'}
-        prediction = make_prediction(data)
-        return {"Prediction": prediction}
-    finally:
-        del frame
+   print("Welcome to FAST API")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
