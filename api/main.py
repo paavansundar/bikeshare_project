2a2177f9848696af6ca551504a70933192908996
paavansundar <paavan.duvvuri@gmail.com>
2023-07-10 15:01:35 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -4,9 +4,13 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get("/predict/")
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/predict")
 def predict():
-   print("Welcome to FAST API")
+   print("Predicted")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
